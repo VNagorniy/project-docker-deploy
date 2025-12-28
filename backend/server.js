c6d4b8e100d3c05aca83c7aee8c46015806b24ac
VNagorniy <vnagorniyrnd@gmail.com>
2025-12-28 22:03:03 +0300

    feat: change name title and console.log

--- backend/server.js
+++ backend/server.js
@@ -11,35 +11,37 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
-  const end = responseTimeHistogram.startTimer();
-  res.on('finish', () => {
-    const routePath = req.route?.path || req.originalUrl || req.path;
+	const end = responseTimeHistogram.startTimer();
+	res.on('finish', () => {
+		const routePath = req.route?.path || req.originalUrl || req.path;
 
-    httpRequestCounter.inc({
-      method: req.method,
-      route: routePath,
-      status: res.statusCode
-    });
+		httpRequestCounter.inc({
+			method: req.method,
+			route: routePath,
+			status: res.statusCode
+		});
 
-    end({
-      method: req.method,
-      route: routePath,
-      status: res.statusCode
-    });
-  });
-  next();
+		end({
+			method: req.method,
+			route: routePath,
+			status: res.statusCode
+		});
+	});
+	next();
 });
 
 app.get('/api/metrics', async (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(await register.metrics());
+	res.set('Content-Type', register.contentType);
+	res.end(await register.metrics());
 });
 // Middleware
 app.use(helmet());
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(
+	cors({
+		origin: '*',
+		credentials: true
+	})
+);
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -49,20 +51,20 @@ app.use('/api/products', productsRoutes);
 
 // Health check
 app.get('/api/health', (req, res) => {
-  res.json({ status: 'OK', message: 'Сервер работает!' });
+	res.json({ status: 'OK', message: 'Сервер работает!' });
 });
 
 // Error handling middleware
 app.use((err, req, res, _next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    error: 'Что-то пошло не так!',
-    message: err.message
-  });
+	console.error(err.stack);
+	res.status(500).json({
+		error: 'Что-то пошло не так!',
+		message: err.message
+	});
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Сервер запущен на порту ${PORT}`);
-  console.log(`📊 API доступен по адресу: http://localhost:${PORT}/api`);
-  console.log('📊 API Version: 1.0.2');
+	console.log(`🚀 Сервер запущен на порту ${PORT}`);
+	console.log(`📊 API доступен по адресу: http://localhost:${PORT}/api`);
+	console.log('📊 API Version: 1.0.4');
 });
